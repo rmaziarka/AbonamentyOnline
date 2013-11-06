ab80f0721b7fafd875e228c09bdccae63463c91c
Radoslaw Maziarka <maziarka.radoslaw@gmail.com>
2013-11-06 18:46:59 +0100

    Breadcrumb

--- Abon.Database/AbonContext.cs
+++ Abon.Database/AbonContext.cs
@@ -20,6 +20,7 @@ namespace Abon.Database
 
         public AbonContext()
         {
+            Configuration.LazyLoadingEnabled = false;
             Database.Connection.ConnectionString =
                 @"Data Source=SIARAPC\SQLEXPRESS;Initial Catalog=Abon;Integrated Security=True;";
            
---
+++ Abon/Scripts/app/Portal/controllers/breadcrumbsController.js
@@ -0,0 +1,29 @@
+﻿controllerModule
+    .controller('breadcrumbsController', ['$scope', 'scopeHelper', function ($scope, helper) {
+        
+        helper.addDataFromProperty($scope, 'userOffersData', 'selectedCategory');
+
+        function createParentList() {
+            $scope.parentCategories = [];
+
+            var category = $scope.selectedCategory;
+
+            while (category.parentId != null) {
+                category = category.parent;
+                $scope.parentCategories.unshift(category);
+            }
+
+        }
+
+        $scope.categoryClicked = function(categoryId) {
+            $scope.$emit('offers-categoryClicked', categoryId);
+        };
+
+        $scope.$on('offers-offersReceived', function (event, data) {
+            $scope.selectedCategory = data.selectedCategory;
+            createParentList();
+        });
+
+        createParentList();
+
+    }]);
